8ccb5321dcc9905e8f8aa1d1671db1a130a136bb
daneeskripter <imdanee@seznam.cz>
2024-01-03 17:17:16 +0100

    novy system odkazu na soubory/slozky

--- server.js
+++ server.js
@@ -1,21 +1,52 @@
 const express = require("express")
 const app = express()
 const fs = require("fs")
+const path = require("path")
 
-app.use("/1rocnik", express.static(__dirname + "/1rocnik"))
+app.set("view engine", "ejs")
 
-app.get("/1rocnik", function (req, res) {
-    res.sendFile(__dirname + "/views/1rocnik.html")
+app.use("/", express.static(__dirname + "/zapisky"))
+
+function getDirectoryContent(directoryPath, callback) {
+  fs.readdir(directoryPath, (err, files) => {
+    if (err) {
+      return callback(err, null);
+    }
+
+    const fileList = [];
+    const folderList = [];
+
+    files.forEach((file) => {
+      const fullPath = path.join(directoryPath, file);
+      const fileStats = fs.statSync(fullPath);
+
+      if (fileStats.isDirectory()) {
+        folderList.push(file);
+      } else {
+        fileList.push(file);
+      }
+    });
+
+    const content = {
+      folders: folderList,
+      files: fileList,
+    };
+
+    callback(null, content);
+  });
+}
+
+app.get("/*", function (req, res) {
+      const currentPath = path.join(__dirname, `/zapisky/${req.params[0]}`);
+if (!currentPath.includes("favicon.ico")) {
+  getDirectoryContent(currentPath, (err, content) => {
+    if (err) {
+      res.status(404).send("No directory or file found")
+    }
+    res.render(__dirname + "/views/index.ejs", { content: content, currentPath: currentPath, path: path, __dirname : __dirname, currentFolder: req.params[0]});
+  })
+}
 })
-
-app.get("/", function (req, res) {
-    res.sendFile(__dirname + "/views/index.html")
-})
-
-app.get("*", function (req, res) {
-    res.redirect("/")
-})
-
 app.listen(80, () => {
     console.log("Server running on port 80")
 })
