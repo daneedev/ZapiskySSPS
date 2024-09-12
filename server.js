d6e59438e529c1fcc8ce2c89bcf1762ef636cd57
daneeskripter <imdanee@seznam.cz>
2024-09-12 17:15:53 +0200

    ejs to nunjucks

--- server.js
+++ server.js
@@ -1,42 +1,21 @@
 const express = require("express")
 const app = express()
-const fs = require("fs")
 const path = require("path")
+const nunjucks = require("nunjucks")
 
-app.set("view engine", "ejs")
+nunjucks.configure("views", {
+    autoescape: true,
+    express: app,
+    watch: true
+})
+
+app.set('view engine', 'njk');
 
 app.use("/", express.static(__dirname + "/zapisky"))
 
-function getDirectoryContent(directoryPath, callback) {
-  fs.readdir(directoryPath, (err, files) => {
-    if (err) {
-      return callback(err, null);
-    }
 
-    const fileList = [];
-    const folderList = [];
 
-    files.forEach((file) => {
-      const fullPath = path.join(directoryPath, file);
-      const fileStats = fs.statSync(fullPath);
-
-      if (fileStats.isDirectory()) {
-        folderList.push(file);
-      } else {
-        fileList.push(file);
-      }
-    });
-
-    const content = {
-      folders: folderList,
-      files: fileList,
-    };
-
-    callback(null, content);
-  });
-}
-
-app.get("/*", function (req, res) {
+/*app.get("/*", function (req, res) {
       const currentPath = path.join(__dirname, `/zapisky/${req.params[0]}`);
 if (!currentPath.includes("favicon.ico")) {
   getDirectoryContent(currentPath, (err, content) => {
@@ -46,7 +25,8 @@ if (!currentPath.includes("favicon.ico")) {
     res.render(__dirname + "/views/index.ejs", { content: content, currentPath: currentPath, path: path, __dirname : __dirname, currentFolder: req.params[0]});
   })
 }
-})
+})*/
+app.get("/", require("./routes/home"))
 app.listen(80, () => {
     console.log("Server running on port 80")
 })
